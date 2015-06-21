b8f063d3c0585fd0b69a966da0fd8b3e754562ca
Nuwinda <stepzero.solutions.nz@gmail.com>
2015-06-21 17:18:39 +1200

    New Updates

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,7 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application','ZbeCore','ZbeAdmin','ZfcBase','ZfThemes'
+        'Application','ZbeCore','ZbeAdmin'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
